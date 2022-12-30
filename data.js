0722b42881017469365e763f8eb664f545dcd261
Valarie Oyieke <oyiekeav@gmail.com>
2022-12-30 08:51:48 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        foodPic: "./images/pizza.png",
+        ingredients: ["pepperoni", "mushroom", "mozarella"],
+        price: 14,
+        emoji: "🍕",
+        id: 0,
+        quantity: 0,
+        total: 0
+    },
+    {
+        name: "Burger",
+        foodPic: "./images/burger.png",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1,
+        quantity: 0,
+        total: 0
+    },
+    {
+        name: "Beer",
+        foodPic: "./images/beer.png",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        id: 2,
+        quantity: 0,
+        total: 0
+    },
+]
